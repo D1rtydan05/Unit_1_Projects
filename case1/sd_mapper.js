1952dc3fd72c95e59c44531890c6e6d24932ef5b
Daniel Garcia <dgarcia300@west-mec.org>
2020-01-16 17:51:24 -0700

    Case 1 done
    ehhh kinda

---
+++ case1/sd_mapper.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 1
+
+   Planisphere Script
+   Author: Daniel Garcia
+   Date:   1-16-20
+
+*/
+
+// uhhh this is for the date
+var thisTime = new Date("2/3/2018, 3:15:28 AM");
+
+var timeStr = thisTime.toLocaleString();
+
+document.getElementById("timeStamp").innerHTML = timeStr;
+
+
+// this separates the current time into hours and months
+var thisHour = thisTime.getHours();
+
+var thisMonth = thisTime.getMonth();
+
+
+// Help me im stuck
+var mapNum = (2 * thisMonth + thisHour) % 24;
+
+var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
+
+document.getElementById("planisphere").insertAdjacentElement("afterbegin",imgStr);
+
